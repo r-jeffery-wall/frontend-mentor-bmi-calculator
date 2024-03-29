c794d8c7eb4b13dffdc063e110ee26e48079177a
r-jeffery-wall <rowjw97@gmail.com>
2024-03-29 16:17:22 +0000

    Added state and units toggle logic.

--- bmi-calculator/src/components/BMI_Calculator.js
+++ bmi-calculator/src/components/BMI_Calculator.js
@@ -1,27 +1,38 @@
+import {useState} from 'react';
+
 export const BMI_Calculator = () => {
-  return (
+    const [units, setUnits] = useState('metric');
+    const [heightCms, setHeightCms] = useState(0);
+    const [weightKgs, setWeightKgs] = useState(0);
+    const [BMI, setBMI] = useState(0);
+
+    const toggleUnits = (event) => {
+        setUnits(event.target.value);
+    }
+    
+    return (
     <div className="bmi-calculator">
       <h3>Enter your details below</h3>
       <form>
         <div id="radio-metric" className="input-group-radio">
-          <input type="radio" id="metric" checked />
+          <input type="radio" id="metric" value="metric" checked={units === 'metric'} name='units' onChange={toggleUnits}/>
           <label for="metric">Metric</label>
         </div>
         <div id="radio-imperial" className="input-group-radio">
-          <input type="radio" id="imperial" />
+          <input type="radio" id="imperial" value="imperial" checked={units === 'imperial'} name='units' onChange={toggleUnits} />
           <label for="imperial">Imperial</label>
         </div>
         <div id="input-height" className="input-group-number">
           <label for="height">Height</label>
           <div className="input-unit">
-            <input type="number" id="height" />
+            <input type="number" id="height" value={heightCms} onChange={(e) => setHeightCms(e.target.value)}/>
             <p className="unit">cm</p>
           </div>
         </div>
         <div id="input-weight" className="input-group-number">
           <label for="weight">Weight</label>
           <div className="input-unit">
-          <input type="number" id="weight" />
+          <input type="number" id="weight" value={weightKgs} onChange={(e) => setWeightKgs(e.target.value)}/>
           <p className="unit">kg</p>
           </div>
         </div>
