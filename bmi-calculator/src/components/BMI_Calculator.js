4dfe0f6885a0c5432cf5c7485fc7d36ba6cf6b1e
r-jeffery-wall <rowjw97@gmail.com>
2024-03-30 12:33:48 +0000

    Added logic for calculating ideal weight and BMI category.

--- bmi-calculator/src/components/BMI_Calculator.js
+++ bmi-calculator/src/components/BMI_Calculator.js
@@ -11,7 +11,8 @@ export const BMI_Calculator = () => {
     const [heightIn, setHeightIn] = useState(0);
     const [weightSt, setWeightSt] = useState(0);
     const [weightLbs, setWeightLbs] = useState(0);
-    const [idealWeight, setIdealWeight] = useState([0, 0]);
+    const [idealWeightMetric, setIdealWeightMetric] = useState([0, 0]);
+    const [category, setCategory] = useState('');
     const [BMI, setBMI] = useState(0);
 
     const toggleUnits = (event) => {
@@ -27,11 +28,34 @@ export const BMI_Calculator = () => {
         }
     }
 
+    const chooseCategory = () => {
+      if (BMI < 18.5) {
+        return 'underweight';
+      } else if (BMI <= 24.9) {
+        return 'a healthy weight';
+      } else if (BMI <= 29.9) {
+        return 'overweight'
+      } else {
+        return 'obese';
+      }
+    }
+
+    const calculateIdealWeightMetric = () => {
+      const lowerBound = 18.5 * ((heightCms / 100) ** 2);
+      const upperBound = 24.9 * ((heightCms / 100) ** 2);
+      return [lowerBound.toFixed(2), upperBound.toFixed(2)];
+    }
+
     useEffect(() => {
         if (heightCms && weightKgs) {
             calculateBMI()
         }
-    })
+    }, [heightCms, weightKgs, heightFt, heightIn, weightSt, weightLbs, units])
+
+    useEffect(() => {
+      setCategory(chooseCategory());
+      setIdealWeightMetric(calculateIdealWeightMetric());
+    }, [BMI])
     
     return (
     <div className="bmi-calculator" id={units}>
